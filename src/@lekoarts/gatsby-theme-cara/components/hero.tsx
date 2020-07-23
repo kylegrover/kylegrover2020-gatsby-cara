f4595556462f749118898bc8038421ff11ce2cf7
kylegrover <kyle@kylegrover.com>
2020-07-23 16:38:07 -0700

    svgs: netlify eleventy jekyll

--- src/@lekoarts/gatsby-theme-cara/components/hero.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/hero.tsx
@@ -15,15 +15,15 @@ const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
         <SVG icon="wordpress" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="20%" />
         <SVG icon="php" width={64} stroke color="icon_red" left="60%" top="70%" />
         <SVG icon="shopify" width={6} color="icon_darker" left="60%" top="15%" />
+        <SVG icon="sass" width={32} stroke color="icon_pink" left="68%" top="25%" />
       </UpDown>
       <UpDownWide>
         <SVG icon="github" hiddenMobile width={16} color="icon_blue" left="80%" top="10%" />
         <SVG icon="css3" width={12} stroke color="icon_brightest" left="90%" top="50%" />
-        <SVG icon="circle" width={16} color="icon_darker" left="70%" top="90%" />
+        <SVG icon="netlify" width={16} color="icon_darker" left="70%" top="90%" />
         <SVG icon="google" width={16} stroke color="icon_darkest" left="30%" top="65%" />
-        <SVG icon="sass" width={32} stroke color="icon_pink" left="28%" top="15%" />
-        <SVG icon="html5" width={6} color="icon_darkest" left="75%" top="10%" />
-        <SVG icon="upDown" hiddenMobile width={8} color="icon_darkest" left="45%" top="10%" />
+        <SVG icon="html5" width={6} color="icon_darkest" left="75%" top="1%" />
+        <SVG icon="jekyll" hiddenMobile width={32} color="icon_darkest" left="35%" top="10%" />
       </UpDownWide>
       <SVG icon="stackoverflow" hiddenMobile width={24} color="icon_darker" left="5%" top="70%" />
       <SVG icon="circle" width={6} color="icon_darkest" left="4%" top="20%" />
@@ -33,8 +33,8 @@ const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
       <SVG icon="triangle" width={8} stroke color="icon_darker" left="25%" top="5%" />
       <SVG icon="circle" width={64} color="icon_green" left="95%" top="5%" />
       <SVG icon="adobe" hiddenMobile width={48} color="icon_purple" left="5%" top="90%" />
-      <SVG icon="box" width={6} color="icon_darkest" left="10%" top="10%" />
-      <SVG icon="box" width={12} color="icon_darkest" left="40%" top="30%" />
+      <SVG icon="eleventy" width={8} color="icon_darkest" left="10%" top="10%" />
+      {/* <SVG icon="box" width={12} color="icon_darkest" left="40%" top="30%" /> */}
       <SVG icon="docker" width={48} stroke color="icon_darker" left="-2%" top="45%" />
       <SVG icon="gulp" width={32} stroke color="icon_darker" left="80%" top="90%" />
     </Divider>
